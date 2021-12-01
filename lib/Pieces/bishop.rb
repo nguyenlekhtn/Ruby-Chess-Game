ac731120fede27edd6bc79e9c6699e9d60551db8
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-12-01 10:50:46 +0700

    add local move valid tests

--- lib/Pieces/bishop.rb
+++ lib/Pieces/bishop.rb
@@ -1,12 +1,14 @@
-require_relative '../moves'
+require_relative '../diagonal'
 require_relative '../piece'
 
 class Bishop < Piece
+  include Diagonal
+
   def post_initialize
     @type = 'bishop'
   end
 
   def local_move_valid?(start:, goal:)
-    Moves.diagonal(start: start, goal: goal)
+    diagonal(start: start, goal: goal)
   end
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -22,7 +22,7 @@ describe Piece do
       end
     end
 
-    context 'when no piece at goal while other condition satisfied' do
+    context 'when no piece at goal while other conditions satisfied' do
       let(:goal) { double('goal') }
 
       before do
@@ -36,7 +36,7 @@ describe Piece do
       end
     end
 
-    context 'when piece at goal has a different color while other condition satisfied' do
+    context 'when piece at goal has a different color while other conditions satisfied' do
       let(:goal) { double('goal') }
 
       before do
@@ -51,7 +51,7 @@ describe Piece do
       end
     end
 
-    context 'when piece at goal has the same color while other condition satisfied' do
+    context 'when piece at goal has the same color while other conditions satisfied' do
       let(:goal) { double('goal') }
 
       before do
@@ -63,6 +63,34 @@ describe Piece do
 
       it 'returns false' do
         expect(piece_move.move_valid?(goal)).to eq(false)
+      end
+    end
+
+    context 'when local move not valid while other conditions satisfied' do
+      let(:goal) { double('goal') }
+
+      before do
+        allow(board).to receive(:empty_at?).with(goal).and_return(true)
+        allow(piece_move).to receive(:in_range?).with(goal).and_return(true)
+        allow(piece_move).to receive(:local_move_valid?).with(start: piece_move.start, goal: goal).and_return(false)
+      end
+
+      it 'returns false' do
+        expect(piece_move.move_valid?(goal)).to eq(false)
+      end
+    end
+
+    context 'when local move valid while other conditions satisfied' do
+      let(:goal) { double('goal') }
+
+      before do
+        allow(board).to receive(:empty_at?).with(goal).and_return(true)
+        allow(piece_move).to receive(:in_range?).with(goal).and_return(true)
+        allow(piece_move).to receive(:local_move_valid?).with(start: piece_move.start, goal: goal).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(piece_move.move_valid?(goal)).to eq(true)
       end
     end
   end
