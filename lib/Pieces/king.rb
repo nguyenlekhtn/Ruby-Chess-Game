0593ef22aecad64e108b4bd8a16f0ae20fbcc7b8
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-12-03 21:09:16 +0700

    include Moves module

--- lib/Pieces/king.rb
+++ lib/Pieces/king.rb
@@ -2,13 +2,15 @@ require_relative '../moves'
 require_relative '../piece'
 
 class King < Piece
+  include Moves
+
   def post_initialize
     @type = 'king'
   end
 
   def local_move_valid?(start:, goal:)
-    Moves.x_by_n(num: 1, start: start, goal: goal) && Moves.y_by_n(num: 0, start: start, goal: goal) ||
-      Moves.x_by_n(num: 0, start: start, goal: goal) && Moves.y_by_n(num: 1, start: start, goal: goal) ||
-      Moves.x_by_n(num: 1, start: start, goal: goal) && Moves.y_by_n(num: 1, start: start, goal: goal)
+    (x_by_n(num: 1, start: start, goal: goal) && y_by_n(num: 0, start: start, goal: goal)) ||
+      (x_by_n(num: 0, start: start, goal: goal) && y_by_n(num: 1, start: start, goal: goal)) ||
+      (x_by_n(num: 1, start: start, goal: goal) && y_by_n(num: 1, start: start, goal: goal))
   end
 end
--- lib/Pieces/knight.rb
+++ lib/Pieces/knight.rb
@@ -2,12 +2,13 @@ require_relative '../moves'
 require_relative '../piece'
 
 class Knight < Piece
+  include Moves
   def post_initialize
     @type = 'knight'
   end
 
   def local_move_valid?(start:, goal:)
-    Moves.x_by_n(num: 1, start: start, goal: goal) && Moves.y_by_n(num: 2, start: start, goal: goal) ||
-      Moves.x_by_n(num: 2, start: start, goal: goal) && Moves.y_by_n(num: 1, start: start, goal: goal)
+    (x_by_n(num: 1, start: start, goal: goal) && y_by_n(num: 2, start: start, goal: goal)) ||
+      (x_by_n(num: 2, start: start, goal: goal) && y_by_n(num: 1, start: start, goal: goal))
   end
 end
