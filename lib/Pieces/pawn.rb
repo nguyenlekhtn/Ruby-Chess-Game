9ef7b8f70996a1d9b6b9558b7062bff9fb7f8658
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-12-04 17:29:39 +0700

    fix pawn due to test

--- lib/Pieces/pawn.rb
+++ lib/Pieces/pawn.rb
@@ -3,42 +3,48 @@ require_relative '../piece'
 
 class Pawn < Piece
   include Lateral
-  attr_reader :color
+  attr_reader :moved
 
-  def post_initialize(**opts)
+  def post_initialize(opts)
     @type = 'pawn'
     @moved = opts[:moved] || false
   end
 
   def local_move_valid?(start:, goal:)
-    return true if @board.empty_at?(goal) && movable_by_color?(start: start, goal: goal)
+    return true if @board.empty_at?(goal) && movable_by_color?(start, goal)
 
-    return true if takable?(start: start, goal: goal)
+    return true if takable?(start, goal)
 
     false
   end
 
   private
 
-  def movable_by_color?(start:, goal:)
+  def movable_by_color?(start, goal)
     neighbours = move_forward_neighbours(start)
-    neighbours.any? { |neighbour| neighbour == goal && vertical_squares_inside_empty?(start: start, goal: goal) }
+    neighbours.any? do |neighbour|
+      distance_vertical = neighbour[0] - start[0]
+      is_piece_between_if_move_2 = (distance_vertical.abs == 2) ?
+                                   vertical_squares_inside_empty?(start: start, goal: goal) :
+                                   true
+      neighbour == goal && is_piece_between_if_move_2
+    end
   end
 
-  def takable?(start:, goal:)
-    take_pieces_squares(start: start).any? { |square| square == goal }
+  def takable?(start, goal)
+    take_pieces_squares(start).any? { |square| square == goal }
   end
 
-  def take_pieces_squares(start:)
+  def take_pieces_squares(start)
     x0, y0 = start
-    [[x0 + 1, move_forward_by_color(y0, 1)], [x0 - 1, move_forward_by_color(y0, 1)]]
+    [[x0 + 1, move_forward_by_color(y0, 1)], [x0 - 1, move_forward_by_color(y0, 1)]].select { |position| in_range?(position) }
   end
 
-  def move_forward_neighbours(start:)
+  def move_forward_neighbours(start)
     x0, y0 = start
     range = [1]
-    range += [2] if 
-    range.map { |num| [x0, move_forward_by_color(y0, num)] }
+    range += [2] unless @moved
+    range.map { |num| [move_forward_by_color(x0, num), y0] }
   end
 
   def move_forward_by_color(num1, step)
